8de6d521e54882aad858dc01dc30fc0b3bf0aab4
serjs <sergey.sablin21@gmail.com>
2020-05-11 11:01:44 +0300

    add logger

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,10 @@
 import { createStore, applyMiddleware } from 'redux'
 import emailReducer from './emails/reducers';
 import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 
-const store = createStore(emailReducer, applyMiddleware(thunk));
+const logger = createLogger({stateTransformer: (state) => state.toJS()})
+
+const store = createStore(emailReducer, applyMiddleware(thunk, logger));
 window['store'] = store;
 export default store;
